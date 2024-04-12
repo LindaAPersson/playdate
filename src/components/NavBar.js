c0ebbf1502d48fbad4174e6c25b06351774f9a5a
Linda Persson <lindapersson-@live.se>
2024-04-12 11:12:59 +0000

    feat>[navbar]add routes to all navbar liinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,21 @@ import React from 'react'
 import {Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
+                <NavLink to="/">
             <Navbar.Brand><img src={logo} alt="logo" height="45"/></Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link>Home <i className="fa-brands fa-fort-awesome"></i></Nav.Link>
-                    <Nav.Link>Sign in <i className="fa-solid fa-arrow-right-to-bracket"></i></Nav.Link>
-                    <Nav.Link>Sign up <i className="fa-solid fa-user-plus"></i></Nav.Link>
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">Home <i className="fa-brands fa-fort-awesome"></i></NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">Sign in <i className="fa-solid fa-arrow-right-to-bracket"></i></NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">Sign up <i className="fa-solid fa-user-plus"></i></NavLink>
                 </Nav>
             </Navbar.Collapse>
             </Container>
