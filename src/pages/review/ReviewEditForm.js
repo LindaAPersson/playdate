dfc64197c454e938b9bbfa4ece8d78a47e4455f6
Linda Persson <lindapersson-@live.se>
2024-04-23 11:50:20 +0000

    <fix>[reviw]upppdate eit review code to make fields visable

--- src/pages/review/ReviewEditForm.js
+++ src/pages/review/ReviewEditForm.js
@@ -4,16 +4,17 @@ import { axiosRes } from "../../api/axiosDefaults";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 
 function ReviewEditForm(props) {
-    const { id, user, created_at, playdate_post, comment, attendance, bring_this, age_recommendation,
-        setShowEditForm,
-        setReviews } = props;
+  const { id, user, created_at, playdate_post, comment, attendance, bring_this, age_recommendation,
+    setShowEditForm,
+    setReviews } = props;
 
-        const [formContent, setFormContent] = useState({
-            comment: "",
-            attendance: "",
-            bring_this: "",
-            age_recommendation: "",
-        });
+  const [formContent, setFormContent] = useState(
+    comment,
+    attendance,
+    bring_this,
+    age_recommendation,
+
+  );
 
   const handleChange = (event) => {
     setFormContent(event.target.value);
@@ -24,16 +25,22 @@ function ReviewEditForm(props) {
     try {
       await axiosRes.put(`/review/${id}/`, {
         comment: formContent.trim(),
+        attendance: formContent.trim(),
+        bring_this: formContent.trim(),
+        age_recommendation: formContent.trim(),
       });
       setReviews((prevComments) => ({
         ...prevComments,
         results: prevComments.results.map((comment) => {
           return comment.id === id
             ? {
-                ...comment,
-                comment: formContent.trim(),
-                updated_at: "now",
-              }
+              ...comment,
+              comment: formContent.trim(),
+              attendance: formContent.trim(),
+              bring_this: formContent.trim(),
+              age_recommendation: formContent.trim(),
+              updated_at: "now",
+            }
             : comment;
         }),
       }));
@@ -45,14 +52,54 @@ function ReviewEditForm(props) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="pr-1">
+      <Form.Group>
+
         <Form.Control
           className={styles.Form}
+          placeholder="my review..."
+          name="comment"
           as="textarea"
           value={formContent}
           onChange={handleChange}
           rows={2}
         />
+
+      </Form.Group>
+      <Form.Group>
+
+        <Form.Check
+          className={styles.Form}
+          type="checkbox"
+          label="Attendance"
+          name="attendance"
+          checked={formContent}
+          onChange={handleChange}
+        />
+
+      </Form.Group>
+      <Form.Group>
+
+        <Form.Control
+          className={styles.Form}
+          placeholder="bring this..."
+          name="bring_this"
+          type="text"
+          value={formContent}
+          onChange={handleChange}
+        />
+
+      </Form.Group>
+      <Form.Group>
+
+        <Form.Control
+          className={styles.Form}
+          placeholder="suitable age..."
+          name="age_recommendation"
+          type="text"
+          value={formContent}
+          onChange={handleChange}
+        />
+
       </Form.Group>
       <div className="text-right">
         <button
@@ -64,7 +111,6 @@ function ReviewEditForm(props) {
         </button>
         <button
           className={styles.Button}
-          disabled={!comment.trim()}
           type="submit"
         >
           save
